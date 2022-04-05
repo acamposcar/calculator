0705c595e7d44f5de8fdeb6e050b6473905d45b6
Alejandro Campos <acamposcar@gmail.com>
2022-04-05 12:21:54 +0200

    Working calculator

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // @ts-check
 
-let currentNumber = '';
-let prevNumber = '';
-let operator = null;
+// Global vars
+let currentNumber;
+let prevNumber;
+let operator;
+let result;
+let waitNumber = true;
 
 function add(a, b) {
   return a + b;
@@ -20,49 +23,99 @@ function divide(a, b) {
   return a / b;
 }
 
+function exponential(a, b) {
+  return a ** b;
+}
 function operate(a, b, oper) {
   switch (oper) {
-    case 'add':
+    case '+':
       return add(a, b);
-    case 'subtract':
+    case '-':
       return subtract(a, b);
-    case 'multiply':
-      return subtract(a, b);
-    case 'divide':
+    case 'x':
+      return multiply(a, b);
+    case '/':
       return divide(a, b);
+    case '^':
+      return exponential(a, b);
+    case 'backspace':
+      return false;
     default:
       return null;
   }
 }
 
-function displayNum() {
-  document.querySelector('#display-number').textContent = currentNumber;
-  document.querySelector('#display-prev').textContent = `${prevNumber} ${operator}`;
+function clearAll() {
+  currentNumber = '';
+  prevNumber = '';
+  result = '';
+  operator = null;
 }
 
+function updateCurrentNum(num) {
+  // Limit the number of numbers that can be displayed on the screen
+  const numberLength = currentNumber.split('').length;
+  if (numberLength <= 9) {
+    currentNumber += num;
+  }
+}
+
+function updatePrevDisplay() {
+  const roundNumber = Math.round(prevNumber * 1000000) / 1000000;
+  document.querySelector('#display-prev').textContent = `${roundNumber}  ${operator || ''}`;
+}
+
+function updateCurrentDisplay() {
+  const roundNumber = Math.round(currentNumber * 1000000) / 1000000;
+  document.querySelector('#display-number').textContent = `${roundNumber}`;
+}
+
+// Operator Keys
 document.querySelectorAll('.key.operator').forEach((key) => {
   key.addEventListener('click', () => {
-    let result = '';
-    if (operator) {
-      result = operate(parseInt(prevNumber, 10), parseInt(currentNumber, 10), operator);
-      prevNumber = currentNumber;
-      currentNumber = result.toString();
-    } else {
-      prevNumber = currentNumber;
+    result = '';
+    if (currentNumber || prevNumber) {
+      if (!waitNumber) {
+        if (operator) {
+          result = operate(parseFloat(prevNumber), parseFloat(currentNumber), operator);
+          currentNumber = result;
+          prevNumber = currentNumber;
+          updateCurrentDisplay();
+        } else {
+          prevNumber = currentNumber;
+        }
+      }
+      waitNumber = true;
+      operator = key.dataset.key;
+      currentNumber = '';
+      if (operator === 'equal') {
+        operator = '';
+        prevNumber = '';
+        updatePrevDisplay();
+        return;
+      }
     }
-    operator = key.dataset.key;
-
-    displayNum();
+    updatePrevDisplay();
   });
 });
 
+// Number Keys
 document.querySelectorAll('.key.number').forEach((key) => {
   key.addEventListener('click', () => {
-    // Limit the number of numbers that can be displayed on the screen
-    const numberLength = currentNumber.split('').length;
-    if (numberLength <= 9) {
-      currentNumber += key.dataset.key;
-    }
-    displayNum();
+    waitNumber = false;
+    updateCurrentNum(key.dataset.key);
+    updateCurrentDisplay();
   });
 });
+
+// AC Key
+document.querySelector('.key[data-key="AC"]').addEventListener('click', () => {
+  clearAll();
+  updateCurrentDisplay();
+  updatePrevDisplay();
+});
+
+// Initiate
+clearAll();
+updateCurrentDisplay();
+updatePrevDisplay();
